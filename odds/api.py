d3602e0895e1c0a2b1fd970116df480b65d2e195
Nick Hale <nickhale@protonmail.ch>
2017-07-24 16:21:10 +0100

    Added Telegram API

---
+++ odds/api.py
@@ -0,0 +1,45 @@
+import requests
+
+from .config import API_URL, test_token
+from .errors import TelegramTokenError, OddsError
+
+class telegram:
+    """
+    Wrapper Class for the Telegram API.
+    Uses requests to get the returned json object.
+    """
+    def __init__(self, token=test_token):
+        """
+        :param token: Telegram API token, Required.
+        """
+        if not token:
+            raise TelegramTokenError
+
+        self.token = token
+        self.params = {}
+
+    def _get(self, api_endpoint, **kwargs):
+        """
+        Method for making a request to Telegram API
+        :param api_endpoint: Telegram API endpoint
+        """
+        req_str = API_URL + self.token + '/' + api_endpoint
+        print(req_str)
+        response = requests.get(url=req_str, params=self.params)
+        return response.json()
+
+    def send_message(self, msg: str, chat_id: str, **kwargs):
+        """
+        :param msg: Message to be sent to users
+        """
+        if kwargs:
+            self.params.update(**kwargs)
+
+        self.params['chat_id'] = chat_id
+        self.params['text'] = msg
+        data = self._get('sendMessage', params=self.params)
+
+        if not data['ok']:
+            raise OddsError(str(data['description']))
+
+        return data['result']
---
+++ odds/errors.py
@@ -0,0 +1,19 @@
+class TelegramTokenError(Exception):
+    msg = 'Missing Telegram token, ' \
+          'visit:https://core.telegram.org/ ' \
+          'to obtain a token'
+
+    def __str__(self):
+        return self.msg
+
+class OddsBaseError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class OddsError(OddsBaseError):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+    def __str__(self):
+        return self.msg
