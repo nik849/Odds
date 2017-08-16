680367a25b23681c9ba2cdcc153907b12bd9c6e8
Nick Hale <nickhale@protonmail.ch>
2017-08-16 14:21:36 +0100

    Added test channel ID to config

--- odds/config.py
+++ odds/config.py
@@ -2,7 +2,7 @@ HOST_URL = 'http://www.asianodds.com/past_200_asian_odds.asp'
 API_URL = 'https://api.telegram.org/'
 WEBHOOK_URL = 'https://5ef47981.ngrok.io'
 test_token = '415496075:AAFSnmY8YQpFbdnrhnIfcbnaF5bDtwV3z1c'
-telegram_id = ['276845594']
+telegram_id = ['276845594', '-1001134247694']
 CONFIG = {"query": None}
 configs = ["config_1", "config_2", "config_3", "config_4", "config_4",
            "config_5", "config_6", "config_7", "config_8", "config_8a",
